6736426c1b67a94c1ece23ed6d4de4468a02c9dd
Matthew Ratzke <matt@degree9.io>
2017-05-07 15:27:56 -0600

    added figlet fonts

---
+++ src/degree9/boot_welcome.clj
@@ -0,0 +1,30 @@
+(ns degree9.boot-welcome
+  (:require [boot.core :as boot]
+            [boot.util :as util]
+            [clojure.java.io :as io]
+            [clj-figlet.core :as fig]))
+
+(defn resource-font [font]
+  (io/resource font))
+
+(defn default-font [font]
+  (resource-font (str "degree9/boot_welcome/fonts/" font)))
+
+(defn file-font [font]
+  (let [f (io/file font)]
+    (when (.exists f)
+      f)))
+
+(boot/deftask welcome
+  "Display a welcome message with  ascii art."
+  []
+  (let [msg  (:message *opts* "Welcome!")
+        font (:font *opts* "isometric2.flf")
+        file (or (file-font font) (resource-font font) (default-font font))]
+    (when-not file
+      (util/warn "Missing Font...: %s \n" font)
+      (util/info msg))
+    (when-let [render (fig/load-flf file)]
+      (util/info (fig/render-to-string render msg))
+      (util/info "\n")))
+  identity)
